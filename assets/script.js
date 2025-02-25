d28ec053a0f184b438bdea369e210e7027f49364
SilkySmooth91 <lorenzo.olivieri13@gmail.com>
2025-02-25 20:44:22 +0100

    getProducts()

--- assets/script.js
+++ assets/script.js
@@ -1,3 +1,23 @@
 // Endpoint 
 
 const productUrl = "https://striveschool-api.herokuapp.com/api/product/"
+
+// DOM Elements
+
+const showProducts = document.getElementById("showProducts")
+
+// Fetch 
+
+async function getProducts() {
+    try {
+        const res = await fetch(productUrl, {
+            headers: {Authorization: "Bearer eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJfaWQiOiI2N2JkZmJjMjFlMTQwNjAwMTUzMTRkMmEiLCJpYXQiOjE3NDA1MDQwMDIsImV4cCI6MTc0MTcxMzYwMn0.-Z5FkCzoME6YmywZCLj5j4nBAzwp8r5KCaTBvAg12-k"}
+        })
+        const products = await res.json()
+        console.log(products)
+    } catch(err) {
+        console.log(err)
+    }
+}
+
+getProducts()
